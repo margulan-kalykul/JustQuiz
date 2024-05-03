85b2754a42360b13635fe9db5b24c5abbfdb3842
margulan-kalykul <m_kalykul@kbtu.kz>
2024-05-03 23:13:02 +0500

    Handlers work. Migrations labeled correctly

--- pkg/quiz/migrations/000001_create_players_table.down.sql
+++ pkg/quiz/migrations/000001_create_players_table.down.sql
@@ -1,27 +1,3 @@
-CREATE TABLE IF NOT EXISTS players (
-    id bigserial PRIMARY KEY,
-    name text NOT NULL,
-    joined timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    last_update timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    score integer NOT NULL DEFAULT 0
-);
-
-CREATE TABLE IF NOT EXISTS quizes (
-    id bigserial PRIMARY KEY,
-	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-	updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-	category  text DEFAULT "",
-	dificulty integer NOT NULL DEFAULT 0
-	q1 text NOT NULL,
-	a1 text NOT NULL,
-	q2 text NOT NULL,
-	a2 text NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS games (
-    id bigserial PRIMARY KEY,
-    time timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    place text,
-    FOREIGN KEY (quiz) REFERENCES quizes(id),
-    FOREIGN KEY (player) REFERENCES player(id)
-);
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS quizes;
+DROP TABLE IF EXISTS games;
--- pkg/quiz/migrations/000001_create_players_table.up.sql
+++ pkg/quiz/migrations/000001_create_players_table.up.sql
@@ -1,3 +1,27 @@
-DROP TABLE IF EXISTS players;
-DROP TABLE IF EXISTS quizes;
-DROP TABLE IF EXISTS games;
+CREATE TABLE IF NOT EXISTS players (
+    id bigserial PRIMARY KEY,
+    name text NOT NULL,
+    joined timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    last_update timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    score integer NOT NULL DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS quizes (
+    id bigserial PRIMARY KEY,
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+	updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+	category  text DEFAULT "",
+	dificulty integer NOT NULL DEFAULT 0
+	q1 text NOT NULL,
+	a1 text NOT NULL,
+	q2 text NOT NULL,
+	a2 text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS games (
+    id bigserial PRIMARY KEY,
+    time timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    place text,
+    FOREIGN KEY (quiz) REFERENCES quizes(id),
+    FOREIGN KEY (player) REFERENCES player(id)
+);
